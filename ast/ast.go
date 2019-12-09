ae1028ee7041d205c9d96e342c750cac7a2819e1
Guillermo <gszeliga@gmail.com>
2019-12-09 12:44:50 +0100

    Add support to prefix expressions

--- ast/ast.go
+++ ast/ast.go
@@ -104,6 +104,25 @@ func (es *ExpressionStatement) String() string {
 	return ""
 }
 
+type PrefixExpression struct {
+	Token    token.Token // the prefix token, e.g. !
+	Operator string
+	Right    Expression
+}
+
+func (pe *PrefixExpression) expressionNode()      {}
+func (pe *PrefixExpression) TokenLiteral() string { return pe.Token.Literal }
+func (pe *PrefixExpression) String() string {
+	var out bytes.Buffer
+
+	out.WriteString("(")
+	out.WriteString(pe.Operator)
+	out.WriteString(pe.Right.String())
+	out.WriteString(")")
+
+	return out.String()
+}
+
 type Identifier struct {
 	Token token.Token
 	Value string
